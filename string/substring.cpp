9e3527e7aa68f3b70a0c77ce00907feb6e7054eb
Sanket Bodhe <74199204+sanketb1102@users.noreply.github.com>
2026-04-14 16:33:27 +0530

    Update substring.cpp

--- string/substring.cpp
+++ string/substring.cpp
@@ -1,1 +1,12 @@
-
+string s = "HELLO";
+int n = s.size();
+for(int i = 0 ;i < n; i++){
+            for(int l = 1; l <= n - i; l++){
+                string str = s.substr(i, l);
+                bool q = checkDup(str);
+                if(q == true){
+                    int currLen = str.length();
+                    ans = max(currLen, ans);
+                }
+            }
+        }
