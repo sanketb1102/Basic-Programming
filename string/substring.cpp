c751f84a4209e0507f8b8faca067b3224f92eedf
Sanket Bodhe <74199204+sanketb1102@users.noreply.github.com>
2026-04-15 17:21:43 +0530

    Update substring.cpp

--- string/substring.cpp
+++ string/substring.cpp
@@ -1,12 +1,9 @@
 string s = "HELLO";
+int cnt = 0;
 int n = s.size();
 for(int i = 0 ;i < n; i++){
             for(int l = 1; l <= n - i; l++){
                 string str = s.substr(i, l);
-                bool q = checkDup(str);
-                if(q == true){
-                    int currLen = str.length();
-                    ans = max(currLen, ans);
-                }
+                cnt++; // just for total no. of substrings possible
             }
-        }
+}
